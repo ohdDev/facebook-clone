ea95701d883ec852c2f45598b73c34697d5a3680
ohdDev <w.ohd95@gmail.com>
2022-04-06 11:17:27 +0300

    edit script

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,7 +1,19 @@
 
 // set active class to top nav bar items
-$(document).on('click', 'nav ul li', function (ev) {
-        ev.preventDefault();
-        $("nav ul li").removeClass("active");
-        $(this).addClass("active");   
-    });
+// $(document).on('click', 'nav ul li', function (ev) {
+//         // ev.preventDefault();
+//         $("nav ul li").removeClass("active");
+//         $(this).addClass("active");   
+//     });
+
+
+// var setDefaultActive = function() {
+//     var path = window.location.pathname;
+
+//     var element = $(".sidebar-menu a[href='" + path + "']");
+
+//     element.addClass("active");
+// }
+
+// setDefaultActive()
+
