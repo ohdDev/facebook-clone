83788dd031ec170a9b71f128b3e6c0dfbe46c87f
Abdulmajeed Alasmari <ialasmariabdulmajeed@gmail.com>
2022-04-06 16:00:05 +0300

    Add arabic lang to registration page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+    around_action :switch_locale
     before_action :configure_permitted_parameters, if: :devise_controller?
     
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,:gender,:birthday,:email])
     end
+
+   def switch_locale(&action)
+        locale = params[:locale] || I18n.default_locale
+        I18n.with_locale(locale, &action)
+   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+<% if I18n.locale == :en %>
+  moment.locale('en');
+<% end %> 
+<% if I18n.locale == :ar %>
+  moment.locale('ar');
+<% end %> 
