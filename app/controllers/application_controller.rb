a94bc4e979b6983da0ae018e0e105a85661d32c4
Abdulmajeed Alasmari <ialasmariabdulmajeed@gmail.com>
2022-04-06 15:12:01 +0300

    adding some style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
+    around_action :switch_locale
+
+    def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+    end
+
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,:gender,:birthday,:email])
     end
+
 end
