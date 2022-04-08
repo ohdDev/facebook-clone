683bee91a2a45c0cd66e20ac44015ed8d84d3af7
Ahlam <aalmutairi@mrsool.co>
2022-04-08 23:50:14 +0300

    edit like view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,7 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
 
-  # GET /likes or /likes.json
-
-  def new
-    @like = Like.new
-  end
-  # GET /likes/1 or /likes/1.json
- 
-  # GET /likes/new
- 
   # POST /likes or /likes.json
   def create
-    p "ddddddddd"
-    p like_params
-    p ",,,,,,,,,,"
-  
     @like = current_user.likes.build(like_params)
     # @like = Like.new(like_params)
 
@@ -30,24 +16,16 @@ class LikesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /likes/1 or /likes/1.json
-
-
   # DELETE /likes/1 or /likes/1.json
   def destroy
+    @like = Like.find(params[:id])
     @like.destroy
-
-    respond_to do |format|
-      format.html { redirect_to likes_url, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to "/posts"
+   
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def like_params
