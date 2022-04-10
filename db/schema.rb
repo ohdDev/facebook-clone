44f0197219c8027b2463ba06e40bb586165f703a
ohdDev <w.ohd95@gmail.com>
2022-04-11 02:54:56 +0300

    edit profiles controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_09_201423) do
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,7 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_201423) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -91,7 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_201423) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -138,12 +136,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_201423) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "events", "users"
-
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
-=======
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-
   add_foreign_key "posts", "users"
 end
